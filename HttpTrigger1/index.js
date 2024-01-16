dd475622c4ef47f4194e2584014b23ff4ba578ca
uchida0627 <v-tuchida@microsoft.com>
2024-01-16 15:26:27 +0900

    my first commit

---
+++ HttpTrigger1/index.js
@@ -0,0 +1,12 @@
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    const name = (req.query.name || (req.body && req.body.name));
+    const responseMessage = name
+        ? "Hello, " + name + ". This HTTP triggered function executed successfully.CI/CDは成功しています"
+        : "This HTTP triggered function executed successfully. CI/CDは成功しています"
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+}
